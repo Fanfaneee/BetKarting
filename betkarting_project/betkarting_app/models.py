42729b1b57340e648baba89099e4356d86b8172f
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-11-17 18:06:31 +0100

    adding model CustomUser

--- betkarting_project/betkarting_app/models.py
+++ betkarting_project/betkarting_app/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=100.0)
+
